bcb7059803c49fffcb5e87d598a90dc4f8e7c3a0
Georges Guy Gustinvil <gergeslyangus@gmail.com>
2022-10-05 16:02:45 -0300

    Adding users and articles associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: { minimum: 5 ,maximum: 50}
-    validates :description, presence: true, length: { minimum: 10 ,maximum: 100}
+    belogs_to :user
+    validates :title, presence: true, 
+                      length: { minimum: 5 ,maximum: 50}
+    validates :description, presence: true, 
+                            length: { minimum: 10 ,maximum: 100}
 end
