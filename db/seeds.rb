c9b19463492fbe165345a533ef2a9010a634729f
Georges Guy Gustinvil <gergeslyangus@gmail.com>
2022-10-09 14:22:48 -0300

    Adding gravatar profile to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+User.create(username: "Georges", email: "georges@example.com", password_digest: "georges1234")
+User.create(username: "Guy", email: "guy@example.com", password_digest: "guy1234")
+User.create(username: "Gustinvil", email: "gustinvil@example.com", password_digest: "gustinvil1234")
+
+Article.create(title: "First title seed", description: "First body seed", user_id: User.first.id)
+Article.create(title: "Second title seed", description: "Second body seed", user_id: User.find(2).id)
+Article.create(title: "Third title seed", description: "Third body seed", user_id: User.last.id)
