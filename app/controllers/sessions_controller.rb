b36d170d4df24bf226e37bbe466fbc3051a4c3e8
Georges Guy Gustinvil <georgeslyangus@gmail.com>
2022-10-13 10:05:28 -0300

    Login, logout and errors credentials messages are working now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,23 +7,22 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
           session[:user_id] = user.id
-          flash[:notice] = "Logged in successfully"
+          flash[:success] = "Logged in successfully"
           if session[:back]
             redirect_to session[:back]
           else
             redirect_to user
           end
         else
-          flash.now[:alert] = "There was something wrong with your login details"
-          puts "Something wrong"
-          render 'new'
+          flash.now[:danger] = "There was something wrong with your login details"
+          render :new,  status: 422
         end
     end
     
     def destroy
         session[:user_id] = nil
         session[:back]=nil
-        flash[:notice] = "Logged out"
+        flash[:success] = "Logged out"
         redirect_to root_path
     end
 end
