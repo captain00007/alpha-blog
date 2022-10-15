c5eb6fefebed78ead9c966d6d3292f70af873c33
Georges Guy Gustinvil <gergeslyangus@gmail.com>
2022-10-15 20:57:46 -0300

    Adding downcase in search form

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,7 +2,7 @@ class SearchesController < ApplicationController
     def search_articles
         #binding.break
         if params[:query].present?
-            @articles = Article.where("title LIKE ?", "%#{params[:query]}%")
+            @articles = Article.where("title LIKE ?", "%#{params[:query].downcase}%")
             @articles=@articles.paginate(page: params[:page], per_page: 2)
             @query=params[:query]
         end    
